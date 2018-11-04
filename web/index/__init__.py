93bf5f2b2b0ea8ab9f0274dea9bf23e3140761b3
Yi Ge <me@yige.ch>
2018-11-04 04:18:56 +0100

    remove unused code

--- web/index/__init__.py
+++ web/index/__init__.py
@@ -1,22 +1,8 @@
-import os
-
 from flask import Flask
-
-
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_mapping(
-    SECRET_KEY='dev',
-    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-)
-
-
-# ensure the instance folder exists
-try:
-    os.makedirs(app.instance_path)
-except OSError:
-    pass
-
 from . import index
+
+
+app = Flask(__name__)
+
 app.register_blueprint(index.bp)
 app.add_url_rule('/', endpoint='index')
-
