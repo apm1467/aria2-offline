ece36174639c1ce33585c0e3bf4cce8f2b555469
Yi Ge <me@yige.ch>
2018-11-05 04:58:10 +0100

    add 10 sec delay after ctime when detecting

--- web/index/index.py
+++ web/index/index.py
@@ -56,23 +56,23 @@ def get_file_list():
     for name in file_names:
         path = join(base_dir, name)
         stat = os.stat(path)
+        display_name = get_file_display_name(name)
         # download isn't finished if the .aria2 temp file exists
         finished = not (name + '.aria2' in temp_names)
-        display_name = get_file_display_name(name)
+        # give 10 sec tolerance after file creation
+        if time.time() - stat.st_ctime < 10:
+            finished = False
 
-        # rename file with the user given name if download is finished
-        real_name = name
-        if finished and real_name != display_name:
-            os.rename(path, join(base_dir, display_name))
-            rds.delete(real_name)
-            real_name = display_name
+        file_name = name
+        if finished:
+            file_name = rename_file(name)
 
         files.append({
-            'name': real_name,
+            'name': file_name,
             'display_name': display_name,
             'size': convert_bytes(stat.st_size),
             'mtime': time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(stat.st_mtime)),
-            'url': settings.FILE_DOWNLOAD_URL + real_name,
+            'url': settings.FILE_DOWNLOAD_URL + file_name,
             'finished': finished
         })
 
@@ -81,6 +81,15 @@ def get_file_list():
 def get_file_display_name(file_name):
     if rds.exists(file_name):
         return rds.get(file_name)
+    return file_name
+
+def rename_file(file_name):
+    if rds.exists(file_name):
+        new_name = rds.get(file_name)
+        base_dir = settings.FILE_BASE_DIR
+        os.rename(join(base_dir, file_name), join(base_dir, new_name))
+        rds.delete(file_name)
+        return new_name
     return file_name
 
 def convert_bytes(num):
